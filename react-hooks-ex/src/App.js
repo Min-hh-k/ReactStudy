6863b3e10622dcf1328ed340afdc5dfb0b0f6548
Minhhk <alszn2222@gmail.com>
2022-05-04 20:28:13 +0900

    custom hooks, useWindowWidth

--- react-hooks-ex/src/App.js
+++ react-hooks-ex/src/App.js
@@ -1,12 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Example1 from "./components/Example1";
-import Example2 from "./components/Example2";
-import Example3 from "./components/Example3";
-import Example4 from "./components/Example4";
+// import Example1 from "./components/Example1";
+// import Example2 from "./components/Example2";
+// import Example3 from "./components/Example3";
+// import Example4 from "./components/Example4";
 import Example5 from "./components/Example5";
+import useWindowWidth from "./hooks/UseWindowWidth";
 
 function App() {
+	const width = useWindowWidth();
+
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -16,6 +19,7 @@ function App() {
 				<Example3 />
 				<Example4 /> */}
 				<Example5 />
+				{width}
 			</header>
 		</div>
 	);

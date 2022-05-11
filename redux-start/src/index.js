a458c909e83b7033a149eeeedf803bac8b571200
Minhhk <alszn2222@gmail.com>
2022-05-11 15:01:07 +0900

    redux combine

--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import store from "./redux/store";
-import { addTodo } from "./redux/actions;";
+import { addTodo, completeTodo, showComplete } from "./redux/actions";
 
 store.subscribe(() => {
 	console.log(store.getState());
 });
 
 // console.log(store);
+store.dispatch(addTodo("할일"));
+store.dispatch(completeTodo(0));
+store.dispatch(showComplete());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
